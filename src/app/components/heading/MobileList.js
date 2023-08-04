eb4ac570ea460aa08bac470a0193837f0e754b5e
Youssef <youssef7amdi@gmail.com>
2023-08-04 22:24:11 +0300

    Updating

--- src/app/components/heading/MobileList.js
+++ src/app/components/heading/MobileList.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 
 const MobileList = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -14,22 +14,9 @@ const MobileList = () => {
     { href: "#register", id: "register", name: "التسجيل" },
   ];
   return (
-    <div
-      className={`cursor-pointer fixed w-full h-full md:hidden top-0 left-0 z-50 transition-all duration-500 ${
-        isOpen
-          ? "before:absolute before:w-full before:h-full before:top-0 before:left-0 before:transition-all before:duration-500 before:bg-black before:opacity-70"
-          : ""
-      }`}
-      onClick={(e) => {
-        if (
-          !e.target.classList.contains("button") &&
-          !e.target.classList.contains("link")
-        )
-          setIsOpen(false);
-      }}
-    >
+    <>
       <button
-        className={`button absolute bottom-8 right-8 text-white rounded-full border-2 w-[70px] h-[70px] text-sm font-bold bg-red-400 transition-all duration-300 hover:opacity-100 ${
+        className={`button fixed z-[100] bottom-8 right-8 text-white rounded-full border-2 w-[70px] h-[70px] text-sm font-bold bg-red-400 transition-all duration-300 hover:opacity-100 ${
           isOpen
             ? "bg-red-600 opacity-100 shadow-lg shadow-red-200"
             : "opacity-40"
@@ -39,7 +26,7 @@ const MobileList = () => {
         القائمة
       </button>
       <ul
-        className={`transition-all duration-300 ${isOpen ? "block" : "hidden"}`}
+        className={`transition-all duration-500 ${isOpen ? "block" : "hidden"}`}
       >
         {links.map((l) => (
           <li key={l.id} className={`link link-${l.id}`}>
@@ -49,7 +36,21 @@ const MobileList = () => {
           </li>
         ))}
       </ul>
-    </div>
+      <div
+        className={`cursor-pointer fixed w-full h-full md:hidden top-0 left-0 transition-all duration-500 ${
+          isOpen
+            ? "z-50 before:absolute before:w-full before:h-full before:top-0 before:left-0 before:transition-all before:duration-500 before:bg-black before:opacity-70"
+            : "z-[-10]"
+        }`}
+        onClick={(e) => {
+          if (
+            !e.target.classList.contains("button") &&
+            !e.target.classList.contains("link")
+          )
+            setIsOpen(false);
+        }}
+      ></div>
+    </>
   );
 };
 
